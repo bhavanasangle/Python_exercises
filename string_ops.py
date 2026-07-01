2a500bcbceb7a49e997b78cc76ef261c704d208e
Bhavana <bhavanasangle@gmail.com>
2026-07-01 20:17:25 +0530

    add string operation

---
+++ string_ops.py
@@ -0,0 +1,20 @@
+# Assignment:
+# p = 'I love my India'
+
+# Access my, India, I love, ove, love my, my India
+s = "PYTHON"
+#conver str in lower case
+s.lower()
+print(s)
+
+#Capitalize method for First Later Capital
+s.capitalize()
+'bhavana sangle'.title()
+#id("bhavana sangle")
+print(s)
+
+# To remove spaces in suffix and prefix use strip() method
+'        python    '.strip()
+'*****JAVA ******'.strip('*')
+"J A V A".strip()
+print(s)
