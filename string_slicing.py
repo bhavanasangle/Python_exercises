7dec3373577b0e55082a59513ecff10ffec9c426
Bhavana <bhavanasangle@gmail.com>
2026-07-02 21:05:56 +0530

    add string slicing operation

---
+++ string_slicing.py
@@ -0,0 +1,20 @@
+#Assignmemt
+p = 'I love my India'
+
+#Access my
+p[7:9]
+print(p[7:9])
+
+#Access India
+p[-5::1]
+print(p[-5::1])
+
+#Access I love
+p[:6]
+print(p[:6])
+
+#Access my India
+p [-8:]
+print(p [-8:])
+
+
